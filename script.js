308d5cf5d99187bd90540249712a771f19c9fa04
elin6-1734094 <49255774+elin6-1734094@users.noreply.github.com>
2019-04-22 17:13:10 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+var fullName = firstName + " " + lastName
+
+console.log("Your full name is " + fullName);
+console.log("You are " + age + " years old");
+
+//<button type="button" onclick="document.write(5 + 6)">Try it</button>
+//
+//document.write("Nice to meet you");
+//document.write(firstName + lastName);
